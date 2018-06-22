b412a351d3cfcb3faa9ab612286d58b6796fe2b1
D070297 <selim.karabulut@sap.com>
2018-06-22 18:15:19 +0200

    Uploading the Project

--- src/main/java/de/hsmannheim/igt/exercise/config/DatabaseConfig.java
+++ src/main/java/de/hsmannheim/igt/exercise/config/DatabaseConfig.java
@@ -2,7 +2,7 @@ package de.hsmannheim.igt.exercise.config;
 
 public class DatabaseConfig {
     // The following String defines the kind of database which should be used
-    public static String CURRENT_DATABASE = "db-cassandra";
+    public static String CURRENT_DATABASE = "db-neo4j";
     
     //db-redis, db-cassandra, db-mongodb, db-neo4j, db_mysql
     
